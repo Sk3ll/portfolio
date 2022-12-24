d1909c180b7239b9a8e2c8a1a1bc0bffe30dda81
Yaroslav Malykhin <yaroslav.malykhin@aimprosoft.com>
2022-12-24 12:48:38 +0200

    feat(nextjs): Init Nextjs, configured eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/return-await': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'class-methods-use-this': 'off',
+    'no-useless-constructor': 'off',
+    'no-return-await': 'off',
+    'new-cap': 'off',
+  },
+};
